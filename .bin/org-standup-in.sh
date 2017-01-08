35938e679feda8d99fb0d7946f49702bfe0777e7
Jean-Martin Archer <jm@jmartin.ca>
2017-01-08 09:19:43 -0800

    org, display closed prs

--- .bin/org-standup-in.sh
+++ .bin/org-standup-in.sh
@@ -6,7 +6,7 @@ set -o pipefail
 source "$HOME/.functions/base"
 source "$HOME/.functions/git"
 
-_format () {
+_format() {
   jq -r '.[] | "** TODO [[\(.html_url)][\(.repository.full_name)#\(.number | tostring) - \(.title)]]" +
 if (has("pull_request") and (.user.login != "j-martin")) then " :review:" else "" end,
 ":PROPERTIES:",
@@ -21,20 +21,29 @@ _get_latest_entry () {
     | cut -d' ' -f2
 }
 
-_generate_done () {
-  _github "issues?assignee=$(_github_user)&state=closed&since=$(_get_latest_entry)" \
+_generate_done() {
+  local user="$(_github_user)"
+  local last_entry="$(_get_latest_entry)"
+
+  _github "issues?assignee=${user}&state=closed&since=${last_entry}" \
     | _format \
     | sed 's/^\*\* TODO/\*\* DONE/' \
     | tr -d '\r'
+
+  _github "issues?creator=${user}&state=closed&since=${last_entry}" \
+    | _format \
+    | sed 's/^\*\* TODO/\*\* DONE/' \
+    | grep ':review:' || echo '' \
+    | sed 's/:review://g' \
+    | tr -d '\r'
 }
 
-_generate_todo () {
+_generate_todo() {
   echo "* $(/bin/date "+%Y-%m-%d")"
   _github "issues?assignee=$(_github_user)" \
     | _format \
     | tr -d '\r'
 }
 
-echo ** --------------------
 _generate_done
 _generate_todo
