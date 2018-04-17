8eb52901b62e5019c3424d7456d54b312cb37c84
Jean-Martin Archer <jm@jmartin.ca>
2018-04-17 12:51:24 -0700

    update iterm open

--- .bin/iterm-open.sh
+++ .bin/iterm-open.sh
@@ -7,11 +7,15 @@ _open() {
   local directory="$1"
 
   osascript &>/dev/null <<EOF
-set the clipboard to "cd '${directory}'"
-tell application "iTerm2" to activate
-tell application "System Events" to tell process "iTerm" to keystroke "t" using {command down}
-tell application "System Events" to tell process "iTerm" to keystroke "v" using {command down}
-tell application "System Events" to tell process "iTerm" to key code 52
+tell application "iTerm"
+  activate
+  tell current window
+    create tab with default profile
+  end tell
+  tell current session of current window
+    write text "cd '${directory}'"
+  end tell
+end tell
 EOF
 }
 
