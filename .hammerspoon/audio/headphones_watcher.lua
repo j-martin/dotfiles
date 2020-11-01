7878ca56a5c810b896a29239f3aa13c8d780af38
Jean-Martin Archer <jm@jmartin.ca>
2020-10-31 22:05:30 -0700

    hs, fix auto speaker mute
    So bluetooth headphones or external DAC works.
    
    Note that there was a bug with the `mod.` prefix infront of the
    functions.

--- .hammerspoon/audio/headphones_watcher.lua
+++ .hammerspoon/audio/headphones_watcher.lua
@@ -1,6 +1,7 @@
 local mod = {}
 
 local audiodevice = require "hs.audiodevice"
+local watcher = require "hs.audiodevice.watcher"
 local alert = require "hs.alert"
 local spotify = require "hs.spotify"
 local logger = hs.logger.new('headphones', 'debug')
@@ -10,13 +11,13 @@ local watchedDevices = {}
 local pluggedFn = nil
 local unpluggedFn = nil
 
-local previousAlert
+local previousTime = 0
 
 local function debounce(message, fn)
   logger.d(message)
-  if message ~= previousAlert then
+  if os.time() - 1 > previousTime then
     alert(message)
-    previousAlert = message
+    previousTime = os.time()
   end
   if fn then
     fn()
@@ -26,22 +27,18 @@ end
 local function audioDeviceWatch(dev_uid, event_name, event_scope, event_element)
   logger.df("Audiodevwatch args: %s, %s, %s, %s", dev_uid, event_name, event_scope, event_element)
   local device = audiodevice.findDeviceByUID(dev_uid)
-  if event_name == 'jack' then
-    if device:jackConnected() then
-      debounce("Headphones plugged", pluggedFn)
-    else
-      debounce("Headphones unplugged → External Speakers muted", unpluggedFn)
-    end
+  if device and device:jackConnected() then
+    debounce("Headphones plugged", mod.pluggedFn)
+  else
+    debounce("Audio Output Change → External Speakers muted", mod.unpluggedFn)
   end
 end
 
 function mod.init(pluggedFn, unpluggedFn)
   mod.pluggedFn = pluggedFn
   mod.unpluggedFn = unpluggedFn
-  local device = audiodevice.findOutputByName('Built-in Output')
-  logger.df("Setting up watcher for audiodevice device %s (UID %s)", device:name(), device:uid())
-  watchedDevices[device:uid()] = device:watcherCallback(audioDeviceWatch)
-  watchedDevices[device:uid()]:watcherStart()
+  watcher.setCallback(audioDeviceWatch)
+  watcher.start()
 end
 
 return mod
