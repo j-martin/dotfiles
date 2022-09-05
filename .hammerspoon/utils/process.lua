0477f1f27c0e464030b8cf63adfec59a2ee27807
Jean-Martin Archer <jm@jmartin.ca>
2022-09-04 22:42:25 -0700

    hs, straggling hs refs

--- .hammerspoon/utils/process.lua
+++ .hammerspoon/utils/process.lua
@@ -1,6 +1,3 @@
-local task = require "hs.task"
-local fnutils = require "hs.fnutils"
-local alert = require "hs.alert"
 local logger = hs.logger.new('execute', 'debug')
 
 local mod = {}
@@ -11,7 +8,7 @@ local env = {PATH = '/usr/local/bin:/usr/bin:/bin', LC_ALL = 'en_US.UTF-8', LANG
 
 local function returnCallback(exitCode, stdOut, stdErr)
   if exitCode ~= 0 then
-    alert.show('the process failed to run, see logs')
+    hs.alert.show('the process failed to run, see logs')
     logger.d(stdOut)
     logger.d(stdErr)
     return false
@@ -49,13 +46,13 @@ local function expand(path)
 end
 
 function mod.start(cmd, args, pwd)
-  running_processes = fnutils.filter(running_processes, cleanupProcess)
+  running_processes = hs.fnutils.filter(running_processes, cleanupProcess)
 
   cmd = expand(cmd)
-  args = fnutils.map(args or {}, expand)
+  args = hs.fnutils.map(args or {}, expand)
   pwd = expand(pwd or '~/.bin')
 
-  process = task.new(cmd, nil, function() end, args)
+  process = hs.task.new(cmd, nil, function() end, args)
   table.insert(running_processes, process)
 
   logger.f('starting %s %s in %s', cmd, args, pwd)
