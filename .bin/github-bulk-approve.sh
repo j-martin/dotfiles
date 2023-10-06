acf11af4ed7cb6faa56ab632e5c034ce6038afec
Jean-Martin Archer <jm@jmartin.ca>
2023-10-06 13:27:33 -0700

    bin, add github-bulk-approve.sh

---
+++ .bin/github-bulk-approve.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env -S pkgx +gh +gum bash@5
+
+set -o errexit
+set -o pipefail
+set -u
+shopt -s inherit_errexit 2> /dev/null
+
+IFS=$'\n\t'
+
+_info() {
+    echo >&2 "[info] $*"
+}
+
+_get() {
+    local json="$1"
+    local path="$2"
+    jq -r "${path}" <<< "${json}"
+}
+
+_main() {
+    local query="${1:-}"
+    local current_user="${2:-}"
+    if [[ -z "${current_user}" ]]; then
+        current_user="$(gh api /user --jq '.login')"
+    fi
+
+    if [[ -n "${query}" ]]; then
+        query="+${query}"
+    fi
+
+    query="is:pr+is:open+review-requested:${current_user}${query}"
+
+    _info "Querying PRs for ${query}..."
+    for pr_json in $(gh api "search/issues?q=${query}" | jq -c '.items[]'); do
+
+        local url
+        url="$(_get "${pr_json}" ".url")"
+        if gum confirm "Approve PR? $(_get "${pr_json}" .title) ${url}"; then
+            local repo
+            repo="$(sed 's#https://api.github.com/repos/##g; s#/issues.*##;' <<< "${url}")"
+            local pr_number
+            pr_number="$(_get "${pr_json}" ".number")"
+
+            gh pr review --repo "${repo}" "${pr_number}" --approve
+        fi
+    done
+}
+
+_main "$@"
