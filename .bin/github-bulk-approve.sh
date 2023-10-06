e9429cda44c2d0c5465c1c8c8276ae5724e50ab9
Jean-Martin Archer <jm@jmartin.ca>
2023-10-06 15:01:09 -0700

    bin, approve, fix url decoding
    Since we switched to html_url the format is now different.

--- .bin/github-bulk-approve.sh
+++ .bin/github-bulk-approve.sh
@@ -8,42 +8,40 @@ shopt -s inherit_errexit 2> /dev/null
 IFS=$'\n\t'
 
 _info() {
-    echo >&2 "[info] $*"
+  echo >&2 "[info] $*"
 }
 
 _get() {
-    local json="$1"
-    local path="$2"
-    jq -r "${path}" <<< "${json}"
+  local json="$1"
+  local path="$2"
+  jq -r "${path}" <<< "${json}"
 }
 
 _main() {
-    local query="${1:-}"
-    local current_user="${2:-}"
-    if [[ -z "${current_user}" ]]; then
-        current_user="$(gh api /user --jq '.login')"
+  local query="${1:-}"
+  local current_user="${2:-}"
+  if [[ -z "${current_user}" ]]; then
+    current_user="$(gh api /user --jq '.login')"
+  fi
+
+  if [[ -n "${query}" ]]; then
+    query="+${query}"
+  fi
+
+  query="is:pr+is:open+review-requested:${current_user}${query}"
+
+  _info "Querying PRs for ${query}..."
+  for pr_json in $(gh api "search/issues?q=${query}" | jq -c '.items[]'); do
+    local url
+    url="$(_get "${pr_json}" ".html_url")"
+    if gum confirm "Approve PR? $(_get "${pr_json}" .title) ${url}/files?w=1"; then
+      local repo
+      repo="$(sed 's#.*github.com/##g; s#/pull/.*##;' <<< "${url}")"
+      local pr_number
+      pr_number="$(_get "${pr_json}" ".number")"
+      gh pr review --repo "${repo}" "${pr_number}" --approve
     fi
-
-    if [[ -n "${query}" ]]; then
-        query="+${query}"
-    fi
-
-    query="is:pr+is:open+review-requested:${current_user}${query}"
-
-    _info "Querying PRs for ${query}..."
-    for pr_json in $(gh api "search/issues?q=${query}" | jq -c '.items[]'); do
-
-        local url
-        url="$(_get "${pr_json}" ".html_url")"
-        if gum confirm "Approve PR? $(_get "${pr_json}" .title) ${url}/files?w=1"; then
-            local repo
-            repo="$(sed 's#.*/repos/##g; s#/issues.*##;' <<< "${url}")"
-            local pr_number
-            pr_number="$(_get "${pr_json}" ".number")"
-
-            gh pr review --repo "${repo}" "${pr_number}" --approve
-        fi
-    done
+  done
 }
 
 _main "$@"
